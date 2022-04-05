098865b840487f84b4ac12de5d24a9935c1efd87
sean <sean@gomicorp.com>
2022-04-05 18:20:22 +0900

    vite & antd 사용을 위한 package 설치 및 설정 변경

--- vite.config.ts
+++ vite.config.ts
@@ -4,28 +4,25 @@ import * as fs from 'fs';
 import lessToJS from 'less-vars-to-js';
 import { resolve } from 'path';
 import { defineConfig } from 'vite';
-import vitePluginImp from 'vite-plugin-imp';
+import { AntdResolve, createStyleImportPlugin } from 'vite-plugin-style-import';
 
 const pathResolver = (path: string) => resolve(__dirname, path);
 const themeVariables = lessToJS(
-  fs.readFileSync(pathResolver('./src/config/styles/variables.less'), 'utf8')
+  fs.readFileSync(pathResolver('./src/styles/variables.less'), 'utf8')
 );
-
 
 // https://vitejs.dev/config/
 export default defineConfig({
   base: './',
   plugins: [
     react(),
-    vitePluginImp({
-      libList: [
+    createStyleImportPlugin({
+      resolves: [AntdResolve()],
+      libs: [
         {
-          libName: 'antd',
-          style: (name) => {
-            if (name === 'col' || name === 'row') {
-              return 'antd/lib/style/index.less';
-            }
-            return `antd/es/${name}/style/index.less`;
+          libraryName: 'antd',
+          resolveStyle: (name) => {
+            return `antd/es/${name}/style/index`;
           },
         },
       ],
